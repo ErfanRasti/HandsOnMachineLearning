83dcec7315ac2c5709178f58a500cacf084a3bac
Erfan Rasti <erfanrasty@gmail.com>
2021-07-09 15:21:54 +0430

    Creating Chap2Housing.py and Chap2Housing.ipynb
    First two files for programming are created.
    
    Chap2Housing.ipynb is for editing with jupyter notebook.
    Chap2Housing.py is for editing with python.
    The final release should be in the Python file.

---
+++ Chap2Housing.py
@@ -0,0 +1,25 @@
+# %%
+"""This code is related to chapter 2 of the book."""
+import os
+import tarfile
+import urllib
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    """
+    Fetch the data to the housing_path.
+
+    housing_url -- url of the housing data
+    housing_path -- an special path to save the data
+    """
+    os.makedirs(housing_path, exist_ok=True)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+# %%
