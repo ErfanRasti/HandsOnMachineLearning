4bcacc1d72b0282e2f6837130706faf51d20e828
Erfan Rasti <erfanrasty@gmail.com>
2021-07-12 18:13:45 +0430

    Downloading And Loading the housing dataset
    I added a function to download the dataset;
    Then I called the function to download the data.
    (This function is called just one time to download the data.)
    
    load_housing_data is added to load the data.
    loaded data is stored in variable housing.

--- Chap2Housing.py
+++ Chap2Housing.py
@@ -1,8 +1,10 @@
 # %%
 """This code is related to chapter 2 of the book."""
+import pandas as pd
 import os
 import tarfile
 import urllib
+# %%
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
 HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
@@ -21,5 +23,32 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     housing_tgz = tarfile.open(tgz_path)
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
+
+
+# Calling the function fetch_housing_data
+# fetch_housing_data()
+# %%
+# loading the data
+
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    """
+    Load the dataset using pandas library.
+
+    Args:
+        housing_path ([string]): Defaults to HOUSING_PATH.
+
+    Returns:
+        <class 'function'>:
+        This function returns a pandas DataFrame object
+        containing all the data.
+    """
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+
+# %%
+housing = load_housing_data()
+housing.head()
 
 # %%
