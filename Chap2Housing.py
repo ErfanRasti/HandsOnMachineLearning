142d9bd0993de31a4630a56f9359e670cfb6e5d9
Erfan Rasti <erfanrasty@gmail.com>
2021-07-13 22:48:10 +0430

    Creating a Test Set
    I test different methods to choose the test set.

--- Chap2Housing.py
+++ Chap2Housing.py
@@ -1,5 +1,9 @@
 # %%
 """This code is related to chapter 2 of the book."""
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import train_test_split
+from zlib import crc32
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
@@ -61,4 +65,88 @@ housing.describe()
 # %%
 housing.hist(bins=50, figsize=(20, 15))
 plt.show()
+# %%
+
+
+def split_train_test(data, test_ratio):
+    """
+    Split test data from the dataset.
+
+    Args:
+        data([DataFrame]): The main dataset
+            containing train data and test data
+
+        test_ratio([float]): the ratio of the test data
+
+    Returns:
+        train set([DataFrame])
+        test set([DataFrame])
+
+
+    This method randomizes  an indices array with numpy random function.
+    The length of array is the same as length of data.
+    Then it calculates test set size and splits indices array
+    to train indices and test indices.
+    Finally it returns the train set and test set.
+    """
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+
+# calling the split_train_test function
+train_set, test_set = split_train_test(housing, 0.2)
+# %%
+len(train_set)
+# %%
+len(test_set)
+# %%
+
+
+def test_set_check(identifier, test_ratio):
+    """Check the identifier with maximum hash value."""
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+
+# %%
+def split_train_test_by_id(data, test_ratio, id_column):
+    """Split the data by id_column."""
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+
+# %%
+housing_with_id = housing.reset_index()  # adds an `index` column
+train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
+# %%
+housing_with_id["id"] = housing["longitude"] * 1000 + housing["latitude"]
+train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "id")
+# %%
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+
+# %%
+housing["income_cat"] = pd.cut(housing["median_income"],
+                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                               labels=[1, 2, 3, 4, 5])
+housing["income_cat"].hist()
+# %%
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    """
+    We mention the target variable in the second argument of split function,
+    to split the data according to this variable.
+    """
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+strat_test_set["income_cat"].value_counts() / len(strat_test_set)
+
+# %%
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
+
 # %%
